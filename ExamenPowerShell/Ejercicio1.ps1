2f74010251e5ee2a74704a09efe9d99e45a3116f
buenasbuenassergio <sergiomartinmmir@gmail.com>
2020-03-13 13:59:50 +0100

    Parte del examen

---
+++ ExamenPowerShell/Ejercicio1.ps1
@@ -0,0 +1,14 @@
+﻿$date = Get-Date 
+
+Set-Location C:\Users\Alumno\Desktop
+$archivos = Get-ChildItem -Path C:\Windows\INF -Force -File  `
+| Where-Object {($_.Name -like "C*" -or $_.Name -like "A*" -or $_.Name -like "M*") -and $_.LastWriteTime.Year -gt "2019"} `
+|ConvertTo-Html -Title "Archivos C:\Windows\INF " -Head "<h1>Listado C:\Windows\INF</h1>" -Property  Name, BaseName, CreationTime, IsReadOnly, Length  |Format-Table -AutoSize  |Out-File output.html  
+
+
+"Autor: " + $env:USERDOMAIN + " Dia " + $date.DayOfWeek +"/" + $date.Month +"/" + $date.Year |Out-File .\output.html -Append
+
+Invoke-Item “output.html"
+
+
+
